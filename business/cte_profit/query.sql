3af00d310c50d1a43ef2da541614c44b1827a00b
Landrykb <landrykassa@gmail.com>
2026-02-27 03:50:36 +0900

    Add business/cte_profit/query.sql — BleepxQuery

---
+++ business/cte_profit/query.sql
@@ -0,0 +1,15 @@
+-- Cte Profit
+-- Domain: Business
+-- My solution query
+
+WITH ReturnTotals AS 
+  (SELECT b.branch, SUM(b.total) AS return_total 
+  FROM business_retail b 
+  JOIN returns r ON b.invoice_id = r.invoice_id 
+  GROUP BY b.branch
+  )
+SELECT b.branch, SUM(b.total) - COALESCE(r.return_total, 0) AS net_profit 
+FROM business_retail b 
+LEFT JOIN ReturnTotals r ON b.branch = r.branch
+GROUP BY b.branch
+ORDER BY net_profit DESC;

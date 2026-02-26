d31ea2c9ec3e231609c0652f7ae96bab8573773a
Landrykb <landrykassa@gmail.com>
2026-02-27 03:50:37 +0900

    Add business/capstone_root/query.sql — BleepxQuery

---
+++ business/capstone_root/query.sql
@@ -0,0 +1,31 @@
+-- Capstone Root
+-- Domain: Business
+-- My solution query
+
+WITH Sales AS (
+  SELECT 
+  product_line, 
+  branch, 
+  SUM(total) AS total_sales
+  FROM business_retail
+  GROUP BY product_line, branch
+),
+ReturnCounts AS (
+  SELECT 
+  b.branch, 
+  b.product_line, 
+  COUNT(*) AS return_count
+  FROM returns r
+  INNER JOIN business_retail b 
+      ON r.invoice_id = b.invoice_id
+  GROUP BY b.branch, b.product_line
+)
+SELECT
+  s.branch,
+  s.product_line,
+  s.total_sales,
+  COALESCE(rc.return_count, 0) AS return_count
+  FROM Sales s
+LEFT JOIN ReturnCounts rc 
+  ON s.branch = rc.branch AND s.product_line = rc.product_line
+ORDER BY total_sales DESC;
